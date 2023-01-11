4608bf4149542881c30ab2900e3c5949ea1b22ce
Jason Prime <600613.m6@gmail.com>
2023-01-11 11:34:29 +0100

    main.py upgraded
    Added improvements
    -Changed the welcome text to "Welcome to PyGen!".
    - I added a for loop > on this line executes the indented code below it as many times as the value of nr_passwords queried in the previous step.
    
    I hope this code upgrade helps you... :)

--- main.py
+++ main.py
@@ -3,29 +3,27 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
+print("Welcome to PyGen!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_passwords = int(input(f"How many passwords would you like to generate?\n"))
 
+for i in range(nr_passwords):
+  password_list = []
 
-password_list = []
+  for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
 
-for char in range(1, nr_letters + 1):
-  password_list.append(random.choice(letters))
+  for char in range(1, nr_symbols + 1):
+    password_list += random.choice(symbols)
 
-for char in range(1, nr_symbols + 1):
-  password_list += random.choice(symbols)
+  for char in range(1, nr_numbers + 1):
+    password_list += random.choice(numbers)
 
-for char in range(1, nr_numbers + 1):
-  password_list += random.choice(numbers)
+  random.shuffle(password_list)
+  password = ""
+  for char in password_list:
+    password += char
 
-print(password_list)
-random.shuffle(password_list)
-print(password_list)
-
-password = ""
-for char in password_list:
-  password += char
-
-print(f"Your password is: {password}")
+  print(f"Your password {i+1} is: {password}")
